9f058b8f46fd830b74983f848bb4abef8137c931
susajhcouvgzuo <131784759+susajhcouvgzuo@users.noreply.github.com>
2023-09-26 09:31:13 -0300

    Add files via upload

---
+++ game_login (2).js
@@ -0,0 +1,9 @@
+// Crie a função addUser()
+function ____ {
+  // Obtenha o valor do usuário através das ids player1_name_input e player2_name_input
+
+  // Armazene esses valores localmente
+
+  // Atribua "game_page.html" para window.location
+}
+
